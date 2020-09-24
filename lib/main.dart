cc0343984bede7d9de6158042d0dfabb94a43c54
ram <ramvishvas.kumar@primathon.in>
2020-09-24 06:31:43 +0530

    Chapter 1 Done

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Awesome App')),
+      body: Container(
+        child: Text(
+          'Hi Flutter',
+        ),
+      ),
+    );
+  }
+}
